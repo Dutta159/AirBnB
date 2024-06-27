80715f44295169d6ff8970760a910c6ae4f62ab9
Yashraj <118662533+Dutta159@users.noreply.github.com>
2024-06-27 21:36:25 +0530

    project phase 1 part B completed

--- models/listing.js
+++ models/listing.js
@@ -9,9 +9,12 @@ const listingSchema = new Schema({
     },
     description : String ,
     image : {
-                type: String,
-                default: "https://images.unsplash.com/photo-1516815231560-8f41ec531527?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
+        type: String,
+        default:
+          "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        set: (v) =>
+          v === ""?"https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60": v    
+          },
     price : Number,
     location : String,
     country : String
