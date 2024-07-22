cffe570be4e67db094090a2a919e4e09c763f3ff
Yashraj <118662533+Dutta159@users.noreply.github.com>
2024-07-22 18:20:01 +0530

    image preview in edit form

--- controllers/listings.js
+++ controllers/listings.js
@@ -42,7 +42,9 @@ module.exports.renderEditForm = async (req,res)=>{
         req.flash("error", "Listing does not exist");
         res.redirect("/listings");
     }
-    res.render("./listings/edit.ejs", {listing});
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
+    res.render("./listings/edit.ejs", {listing,originalImageUrl});
 }
 
 
